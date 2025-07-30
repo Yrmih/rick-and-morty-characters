51ec0b361fae6aaa299cf94ae730b4d21882aa7d
Yrmih <yrmihiangoncalvesdelima@gmail.com>
2025-07-30 16:48:20 -0300

    feat: adicionando os personagens ao mock no arquivo page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,29 @@ const mockCharacters: Character[] = [
     status: "Alive",
     species: "Human",
   },
+  {
+    id: 2,
+    name: "Morty Smith",
+    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    status: "Alive",
+    species: "Human",
+  },
+  {
+    id: 3,
+    name: "Summer Smith",
+    image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    status: "Alive",
+    species: "Human",
+  },
+  {
+    id: 4,
+    name: "Beth Smith",
+    image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+    status: "Alive",
+    species: "Human",
+  },
+  {
+  }
 ]
 
 
