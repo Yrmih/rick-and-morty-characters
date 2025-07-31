53db95ff059bf4805cf7f03a1234f41c3b607987
Yrmih <yrmihiangoncalvesdelima@gmail.com>
2025-07-31 15:30:39 -0300

    feat: remove mock characters data from page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,8 @@
 'use client';
 
 import { Character } from './types/types';
+import { mockCharacters } from './mocks/mocks';
 
-
-const mockCharacters: Character[] = [
-   {
-    id: 1,
-    name: "Rick Sanchez",
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    status: "Alive",
-    species: "Human",
-  },
-  {
-    id: 2,
-    name: "Morty Smith",
-    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-    status: "Alive",
-    species: "Human",
-  },
-  {
-    id: 3,
-    name: "Summer Smith",
-    image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-    status: "Alive",
-    species: "Human",
-  },
-  {
-    id: 4,
-    name: "Beth Smith",
-    image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
-    status: "Alive",
-    species: "Human",
-  },
-   {
-    id: 5,
-    name: "Jerry Smith",
-    image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
-    status: "Alive",
-    species: "Human",
-  },
-  {
-    id: 6,
-    name: "Mr. Meeseeks",
-    image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
-    status: "Alive",
-    species: "Alien",
-  },
-  {
-    id: 7,
-    name: "Birdperson",
-    image: "https://rickandmortyapi.com/api/character/avatar/7.jpeg",
-    status: "Alive",
-    species: "Birdperson",
-  },
-  {
-    id: 8,
-    name: "Squanchy",
-    image: "https://rickandmortyapi.com/api/character/avatar/8.jpeg",
-    status: "Alive",
-    species: "Cat Person",
-  },
-]
 
 
 export default function Home() {
