bae7c7c046285961dcb012f05cb29537cc385ac5
Yrmih <yrmihiangoncalvesdelima@gmail.com>
2025-07-31 14:56:36 -0300

    feat: estiliza botão "Anterior" na página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,7 +99,7 @@ export default function Home() {
     </div>
 
     <div className='mt-8 flex justify-center gap-4'>
-      <button>
+      <button className="px-4 py-2 bg-zinc-800 rounded hover:bg-zinc-700">
         Anterior
       </button>
       <button>
